d7ee816db5ad9ee5800da9bd299513861332a1cd
Angela Schlesinger <schlesin@oceannetworks.ca>
2024-05-15 13:10:28 -0700

    remove private path

--- Calibrate_1024C_TestData.py
+++ Calibrate_1024C_TestData.py
@@ -1,7 +1,7 @@
 import sys, os
 
 # csfp - current_script_folder_path
-csfp = os.path.abspath(os.path.dirname('RK_Stuff/CORK1024C'))
+csfp = os.path.abspath(os.path.dirname('CORK_Stuff/CORK1024C'))
 if csfp not in sys.path:
     sys.path.insert(0, csfp)
 
